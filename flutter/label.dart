4073746d8f5fb3f257b448a19dc4122101b3b199
John Gorter <johngorter@iPhone-van-Leon.desteckgeasten.desteck.nu>
2024-09-30 13:03:31 +0200

    init

--- flutter/label.dart
+++ flutter/label.dart
@@ -1,3 +1,6 @@
 
+//#pragma: imports
+import 'dart:convert';
+
 //#pragma: execution
 GestureDetector(onTap:(){ dynamic event;##TRIGGERS## }, child:Text("${jsonEncode(##SOURCE##})", style: Theme.of(context).textTheme.headlineMedium)),
